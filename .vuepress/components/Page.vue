f353b5b5c2a659f037f5088337f48bb3773fac3d
Maximiliano Molina <hpmaxi@gmail.com>
2022-04-12 16:21:16 -0300

    Remove generator and hardcode values

--- .vuepress/components/Page.vue
+++ .vuepress/components/Page.vue
@@ -15,31 +15,45 @@
       <b-jumbotron bg-variant="light" header-level="4" class="px-3 py-4">
         <template #header>
           <b-media class="mb-3">
-            <b-img v-if="token.logo"
-                   slot="aside"
-                   :src="token.logo"
-                   rounded="circle"
-                   width="64"
-                   height="64"
-                   :alt="token.name"/>
+            <b-img
+              v-if="token.logo"
+              slot="aside"
+              :src="token.logo"
+              rounded="circle"
+              width="64"
+              height="64"
+              :alt="token.name"
+            />
             {{ token.name }}
           </b-media>
         </template>
-        <template #lead>
-        </template>
-        <hr class="my-4">
-        <h6>Name: <span class="text-muted">{{ token.name }}</span></h6>
-        <h6>Symbol: <span class="text-muted">{{ token.symbol }}</span></h6>
-        <h6>Decimals: <span class="text-muted">{{ token.decimals }}</span></h6>
-        <h6>Address: <span class="text-muted token-address">{{ token.address }}</span></h6>
-        <hr class="my-4">
+        <template #lead> </template>
+        <hr class="my-4" />
+        <h6>
+          Nombre: <span class="text-muted">{{ token.name }}</span>
+        </h6>
+        <h6>
+          Símbolo: <span class="text-muted">{{ token.symbol }}</span>
+        </h6>
+        <h6>
+          Decimales: <span class="text-muted">{{ token.decimals }}</span>
+        </h6>
+        <h6>
+          Dirección:
+          <span class="text-muted token-address">{{ token.address }}</span>
+        </h6>
+        <hr class="my-4" />
         <b-link @click="addToMetaMask" class="btn btn-success my-2">
           <b-icon-plus-circle-fill></b-icon-plus-circle-fill>
-          Add to MetaMask
+          Agregar a MetaMask
         </b-link>
-        <b-link :href="token.explorerLink" target="_blank" class="btn btn-primary my-2">
+        <b-link
+          :href="token.explorerLink"
+          target="_blank"
+          class="btn btn-primary my-2"
+        >
           <b-icon-arrow-up-right-circle-fill></b-icon-arrow-up-right-circle-fill>
-          View on {{ network.current.explorerName }}
+          Ver en {{ network.current.explorerName }}
         </b-link>
         <b-link @click="shareToken" class="btn btn-info my-2 float-right">
           <b-icon-share></b-icon-share>
@@ -48,32 +62,31 @@
       <b-modal ref="shareModal" hide-footer :title="`Share ${token.name} page`">
         <b-row>
           <b-col lg="12">
-            <b-form-group
-                label="Share link"
-                label-for="tokenLink">
+            <b-form-group label="Share link" label-for="tokenLink">
               <b-form-input
-                  id="tokenLink"
-                  name="tokenLink"
-                  placeholder="Your token link"
-                  size="lg"
-                  readonly
-                  v-model.trim="share.shortLink">
+                id="tokenLink"
+                name="tokenLink"
+                placeholder="Your token link"
+                size="lg"
+                readonly
+                v-model.trim="share.shortLink"
+              >
               </b-form-input>
             </b-form-group>
           </b-col>
           <b-col lg="12">
             <p class="share-link">
               <b-btn :href="share.twitter" target="_blank" class="twitter">
-                <font-awesome-icon :icon="['fab', 'twitter']"/>
+                <font-awesome-icon :icon="['fab', 'twitter']" />
               </b-btn>
               <b-btn :href="share.facebook" target="_blank" class="facebook">
-                <font-awesome-icon :icon="['fab', 'facebook-f']"/>
+                <font-awesome-icon :icon="['fab', 'facebook-f']" />
               </b-btn>
               <b-btn :href="share.telegram" target="_blank" class="telegram">
-                <font-awesome-icon :icon="['fab', 'telegram-plane']"/>
+                <font-awesome-icon :icon="['fab', 'telegram-plane']" />
               </b-btn>
               <b-btn :href="share.whatsapp" target="_blank" class="whatsapp">
-                <font-awesome-icon :icon="['fab', 'whatsapp']"/>
+                <font-awesome-icon :icon="['fab', 'whatsapp']" />
               </b-btn>
             </p>
           </b-col>
@@ -84,120 +97,126 @@
 </template>
 
 <script>
-  import browser from '../mixins/browser';
-  import dapp from '../mixins/dapp';
-  import shortener from '../mixins/shortener';
+import browser from "../mixins/browser";
+import dapp from "../mixins/dapp";
+import shortener from "../mixins/shortener";
 
-  export default {
-    name: 'Page',
-    mixins: [
-      browser,
-      dapp,
-      shortener,
-    ],
-    data () {
-      return {
-        loaded: false,
-        loading: true,
-        share: {},
-        currentNetwork: null,
-        token: {},
+export default {
+  name: "Page",
+  mixins: [browser, dapp, shortener],
+  data() {
+    return {
+      loaded: false,
+      loading: true,
+      share: {},
+      currentNetwork: null,
+      token: {},
+    };
+  },
+  mounted() {
+    this.currentNetwork = this.getParam("network") || this.network.default;
+    this.initDapp();
+  },
+  methods: {
+    async initDapp() {
+      this.network.current = this.network.list[this.currentNetwork];
+      try {
+        await this.initWeb3(this.currentNetwork, true);
+        await this.getToken(decodeURIComponent(this.getParam("hash")));
+      } catch (e) {
+        alert(e);
+        document.location.href = this.$withBase("/");
+      }
+    },
+    async getToken() {
+      const tokenInfo = {
+        address: "0xdbc6b1cf3042d19c300a0b618b3d59d256d7a448",
+        logo:
+          "https://uca66d3d273c47e9b70101076794.previews.dropboxusercontent.com/p/thumb/ABhplioDyi87weyd9H6zg2Ww3Wlg0WeR-rU-5LNlR8ObyHQDflNMwhxUXqNJ7iZuNifXw4bsRHpwLoouoaWkv11aSrYPzLRPakboimGSaiAGBLRVYmN9ocZ6nT85tF8c_eREwYvLmWzTJoSxZAXJ3PRwdcyCJqzTZYPh0dvWu8-ZOV6chG-sryLjJqA3mQapegqawLbfL-KBWA7mH14W2ScOimti6V_PQXHE5Qr8RmTntDF4NlIvwe8tBJu50womfbaKIxcs0Yn28ufX5xRPXlXR8rnrzE-URUTuZRUOm0u-r5txBFAcrZ3AIYtS01BAhntu4ymn4wNjwe_NM9ljnVW6CH6G7YUJh4r6pyeYL8Met7mXPGrmOUMbdtowwniMFZk/p.png",
       };
+
+      this.token.address = tokenInfo.address;
+      this.initContract(this.token.address);
+
+      this.token.name = await this.promisify(
+        this.instances.token.methods.name().call
+      );
+      this.token.symbol = await this.promisify(
+        this.instances.token.methods.symbol().call
+      );
+      this.token.decimals = (
+        await this.promisify(this.instances.token.methods.decimals().call)
+      ).valueOf();
+      this.token.logo = tokenInfo.logo ? tokenInfo.logo : "";
+
+      if (!this.token.name || !this.token.symbol || !this.token.decimals) {
+        alert("Token inválido o red incorrecta");
+        this.loaded = false;
+      } else {
+        this.token.explorerLink = `${this.network.current.explorerLink}/token/${this.token.address}`;
+
+        this.loaded = true;
+
+        document.title = `${this.token.name} (${this.token.symbol}) | ${this.$page.frontmatter.title}`;
+      }
+
+      this.loading = false;
     },
-    mounted () {
-      this.currentNetwork = this.getParam('network') || this.network.default;
-      this.initDapp();
-    },
-    methods: {
-      async initDapp () {
-        this.network.current = this.network.list[this.currentNetwork];
-        try {
-          await this.initWeb3(this.currentNetwork, true);
-          await this.getToken(decodeURIComponent(this.getParam('hash')));
-        } catch (e) {
-          alert(e);
-          document.location.href = this.$withBase('/');
-        }
-      },
-      async getToken (tokenHash) {
-        if (tokenHash) {
-          const tokenInfo = JSON.parse(this.web3.utils.hexToAscii(tokenHash));
-
-          this.token.address = tokenInfo.address;
-          this.initContract(this.token.address);
-
-          this.token.name = await this.promisify(this.instances.token.methods.name().call);
-          this.token.symbol = await this.promisify(this.instances.token.methods.symbol().call);
-          this.token.decimals = (await this.promisify(this.instances.token.methods.decimals().call)).valueOf();
-          this.token.logo = tokenInfo.logo ? tokenInfo.logo : '';
-
-          if (!this.token.name || !this.token.symbol || !this.token.decimals) {
-            alert('It seems that it is not a valid Token or you are on the wrong network');
-            this.loaded = false;
-          } else {
-            this.token.explorerLink = `${this.network.current.explorerLink}/token/${this.token.address}`;
-
-            this.loaded = true;
-
-            document.title = `${this.token.name} (${this.token.symbol}) | ${this.$page.frontmatter.title}`;
-          }
-
-          this.loading = false;
-        } else {
-          document.location.href = this.$withBase('/');
-        }
-      },
-      async addToMetaMask () {
-        if (!this.metamask.installed) {
-          alert('Please install MetaMask and try again!');
+    async addToMetaMask() {
+      if (!this.metamask.installed) {
+        alert("Please install MetaMask and try again!");
+        return;
+      } else {
+        if (this.metamask.netId !== this.network.current.id) {
+          alert(
+            `Your MetaMask in on the wrong network. Please switch on ${this.network.current.name} and try again!`
+          );
           return;
-        } else {
-          if (this.metamask.netId !== this.network.current.id) {
-            alert(
-              `Your MetaMask in on the wrong network. Please switch on ${this.network.current.name} and try again!`,
-            );
-            return;
-          }
         }
+      }
 
-        try {
-          await this.web3Provider.request({
-            method: 'wallet_watchAsset',
-            params: {
-              type: 'ERC20',
-              options: {
-                address: this.token.address,
-                symbol: this.token.symbol.substr(0, 11),
-                decimals: this.token.decimals,
-                image: this.token.logo,
-              },
+      try {
+        await this.web3Provider.request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC20",
+            options: {
+              address: this.token.address,
+              symbol: this.token.symbol.substr(0, 11),
+              decimals: this.token.decimals,
+              image: this.token.logo,
             },
-          });
-        } catch (e) {
-          console.log(e); // eslint-disable-line no-console
-        }
-      },
-      async shareToken () {
-        const tokenHash = this.web3.utils.toHex(
-          JSON.stringify({
-            address: this.token.address,
-            logo: this.token.logo,
-          }),
-        );
-
-        this.share.tokenLink = window.location.origin + this.$withBase(`/page/?hash=${tokenHash}&network=${this.currentNetwork}`); // eslint-disable-line max-len
-
-        this.share.shortLink = await this.shorten(this.share.tokenLink);
-
-        this.share.facebook = `https://www.facebook.com/sharer.php?u=${this.share.shortLink}&quote=Discover more about ${this.token.name} (${this.token.symbol}).`; // eslint-disable-line max-len
-        this.share.twitter = `https://twitter.com/intent/tweet?url=${this.share.shortLink}&text=Discover more about ${this.token.name} (${this.token.symbol}).`; // eslint-disable-line max-len
-        this.share.telegram = `https://t.me/share/url?url=${this.share.shortLink}&text=Discover more about ${this.token.name} (${this.token.symbol}).`; // eslint-disable-line max-len
-        this.share.whatsapp = `https://wa.me/?text=Discover more about ${this.token.name} (${this.token.symbol}). ${this.share.shortLink}`; // eslint-disable-line max-len
-
-        this.$forceUpdate();
-
-        this.$refs.shareModal.show();
-      },
+          },
+        });
+      } catch (e) {
+        console.log(e); // eslint-disable-line no-console
+      }
     },
-  };
+    async shareToken() {
+      const tokenHash = this.web3.utils.toHex(
+        JSON.stringify({
+          address: this.token.address,
+          logo: this.token.logo,
+        })
+      );
+
+      this.share.tokenLink =
+        window.location.origin +
+        this.$withBase(
+          `/page/?hash=${tokenHash}&network=${this.currentNetwork}`
+        ); // eslint-disable-line max-len
+
+      this.share.shortLink = await this.shorten(this.share.tokenLink);
+
+      this.share.facebook = `https://www.facebook.com/sharer.php?u=${this.share.shortLink}&quote=Discover more about ${this.token.name} (${this.token.symbol}).`; // eslint-disable-line max-len
+      this.share.twitter = `https://twitter.com/intent/tweet?url=${this.share.shortLink}&text=Discover more about ${this.token.name} (${this.token.symbol}).`; // eslint-disable-line max-len
+      this.share.telegram = `https://t.me/share/url?url=${this.share.shortLink}&text=Discover more about ${this.token.name} (${this.token.symbol}).`; // eslint-disable-line max-len
+      this.share.whatsapp = `https://wa.me/?text=Discover more about ${this.token.name} (${this.token.symbol}). ${this.share.shortLink}`; // eslint-disable-line max-len
+
+      this.$forceUpdate();
+
+      this.$refs.shareModal.show();
+    },
+  },
+};
 </script>
--- .vuepress/mixins/dapp.js
+++ .vuepress/mixins/dapp.js
@@ -1,8 +1,8 @@
-import config from '../config';
-import TokenABI from '../abi/TokenABI.json';
+import config from "../config";
+import TokenABI from "../abi/TokenABI.json";
 
 export default {
-  data () {
+  data() {
     return {
       web3: null,
       web3Provider: null,
@@ -12,104 +12,104 @@ export default {
       },
       network: {
         default: config.defaultNetwork,
-        current: null,
+        current: "mainnet",
         map: {
-          1: 'mainnet',
-          3: 'ropsten',
-          4: 'rinkeby',
-          42: 'kovan',
-          5: 'goerli',
-          56: 'bsc_mainnet',
-          97: 'bsc_testnet',
+          1: "mainnet",
+          // 3: "ropsten",
+          // 4: "rinkeby",
+          // 42: "kovan",
+          // 5: "goerli",
+          // 56: "bsc_mainnet",
+          // 97: "bsc_testnet",
         },
         list: {
           mainnet: {
-            blockchain: 'Ethereum',
-            tokenType: 'ERC20',
+            blockchain: "Ethereum",
+            tokenType: "ERC20",
             web3Provider: `https://mainnet.infura.io/v3/${config.infuraProjectId}`,
-            explorerLink: 'https://etherscan.io',
-            explorerName: 'Etherscan',
+            explorerLink: "https://etherscan.io",
+            explorerName: "Etherscan",
             id: 1,
-            name: 'Main Ethereum Network',
+            name: "Main Ethereum Network",
           },
-          ropsten: {
-            blockchain: 'Ethereum',
-            tokenType: 'ERC20',
-            web3Provider: `https://ropsten.infura.io/v3/${config.infuraProjectId}`,
-            explorerLink: 'https://ropsten.etherscan.io',
-            explorerName: 'Etherscan',
-            id: 3,
-            name: 'Ropsten Test Network',
-          },
-          rinkeby: {
-            blockchain: 'Ethereum',
-            tokenType: 'ERC20',
-            web3Provider: `https://rinkeby.infura.io/v3/${config.infuraProjectId}`,
-            explorerLink: 'https://rinkeby.etherscan.io',
-            explorerName: 'Etherscan',
-            id: 4,
-            name: 'Rinkeby Test Network',
-          },
-          kovan: {
-            blockchain: 'Ethereum',
-            tokenType: 'ERC20',
-            web3Provider: `https://kovan.infura.io/v3/${config.infuraProjectId}`,
-            explorerLink: 'https://kovan.etherscan.io',
-            explorerName: 'Etherscan',
-            id: 42,
-            name: 'Kovan Test Network',
-          },
-          goerli: {
-            blockchain: 'Ethereum',
-            tokenType: 'ERC20',
-            web3Provider: `https://goerli.infura.io/v3/${config.infuraProjectId}`,
-            explorerLink: 'https://goerli.etherscan.io',
-            explorerName: 'Etherscan',
-            id: 5,
-            name: 'Goerli Test Network',
-          },
-          bsc_mainnet: { // eslint-disable-line camelcase
-            blockchain: 'Binance Smart Chain',
-            tokenType: 'BEP20',
-            web3Provider: 'https://bsc-dataseed.binance.org/',
-            explorerLink: 'https://bscscan.com',
-            explorerName: 'BscScan',
-            id: 56,
-            name: 'BSC Mainnet',
-          },
-          bsc_testnet: { // eslint-disable-line camelcase
-            blockchain: 'Binance Smart Chain',
-            tokenType: 'BEP20',
-            web3Provider: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-            explorerLink: 'https://testnet.bscscan.com',
-            explorerName: 'BscScan',
-            id: 97,
-            name: 'BSC Testnet',
-          },
-          polygon_mainnet: { // eslint-disable-line camelcase
-            blockchain: 'Polygon',
-            tokenType: 'ERC20',
-            web3Provider: 'https://polygon-rpc.com/',
-            explorerLink: 'https://polygonscan.com',
-            explorerName: 'PolygonScan',
-            id: 137,
-            name: 'Polygon Mainnet',
-          },
-          polygon_mumbai: { // eslint-disable-line camelcase
-            blockchain: 'Polygon',
-            tokenType: 'ERC20',
-            web3Provider: 'https://rpc-mumbai.maticvigil.com/',
-            explorerLink: 'https://mumbai.polygonscan.com',
-            explorerName: 'PolygonScan',
-            id: 80001,
-            name: 'Matic Mumbai',
-          },
+          // ropsten: {
+          //   blockchain: "Ethereum",
+          //   tokenType: "ERC20",
+          //   web3Provider: `https://ropsten.infura.io/v3/${config.infuraProjectId}`,
+          //   explorerLink: "https://ropsten.etherscan.io",
+          //   explorerName: "Etherscan",
+          //   id: 3,
+          //   name: "Ropsten Test Network",
+          // },
+          // rinkeby: {
+          //   blockchain: "Ethereum",
+          //   tokenType: "ERC20",
+          //   web3Provider: `https://rinkeby.infura.io/v3/${config.infuraProjectId}`,
+          //   explorerLink: "https://rinkeby.etherscan.io",
+          //   explorerName: "Etherscan",
+          //   id: 4,
+          //   name: "Rinkeby Test Network",
+          // },
+          // kovan: {
+          //   blockchain: "Ethereum",
+          //   tokenType: "ERC20",
+          //   web3Provider: `https://kovan.infura.io/v3/${config.infuraProjectId}`,
+          //   explorerLink: "https://kovan.etherscan.io",
+          //   explorerName: "Etherscan",
+          //   id: 42,
+          //   name: "Kovan Test Network",
+          // },
+          // goerli: {
+          //   blockchain: "Ethereum",
+          //   tokenType: "ERC20",
+          //   web3Provider: `https://goerli.infura.io/v3/${config.infuraProjectId}`,
+          //   explorerLink: "https://goerli.etherscan.io",
+          //   explorerName: "Etherscan",
+          //   id: 5,
+          //   name: "Goerli Test Network",
+          // },
+          // bsc_mainnet: {
+          //   // eslint-disable-line camelcase
+          //   blockchain: "Binance Smart Chain",
+          //   tokenType: "BEP20",
+          //   web3Provider: "https://bsc-dataseed.binance.org/",
+          //   explorerLink: "https://bscscan.com",
+          //   explorerName: "BscScan",
+          //   id: 56,
+          //   name: "BSC Mainnet",
+          // },
+          // bsc_testnet: {
+          //   // eslint-disable-line camelcase
+          //   blockchain: "Binance Smart Chain",
+          //   tokenType: "BEP20",
+          //   web3Provider: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+          //   explorerLink: "https://testnet.bscscan.com",
+          //   explorerName: "BscScan",
+          //   id: 97,
+          //   name: "BSC Testnet",
+          // },
+          // polygon_mainnet: {
+          //   // eslint-disable-line camelcase
+          //   blockchain: "Polygon",
+          //   tokenType: "ERC20",
+          //   web3Provider: "https://polygon-rpc.com/",
+          //   explorerLink: "https://polygonscan.com",
+          //   explorerName: "PolygonScan",
+          //   id: 137,
+          //   name: "Polygon Mainnet",
+          // },
+          // polygon_mumbai: {
+          //   // eslint-disable-line camelcase
+          //   blockchain: "Polygon",
+          //   tokenType: "ERC20",
+          //   web3Provider: "https://rpc-mumbai.maticvigil.com/",
+          //   explorerLink: "https://mumbai.polygonscan.com",
+          //   explorerName: "PolygonScan",
+          //   id: 80001,
+          //   name: "Matic Mumbai",
+          // },
         },
-        blockchains: [
-          'Ethereum',
-          'Binance Smart Chain',
-          'Polygon',
-        ],
+        blockchains: ["Ethereum"], // , "Binance Smart Chain", "Polygon"],
       },
       contracts: {
         token: null,
@@ -120,15 +120,17 @@ export default {
     };
   },
   methods: {
-    async initWeb3 (network, checkWeb3) {
+    async initWeb3(network, checkWeb3) {
       if (!Object.prototype.hasOwnProperty.call(this.network.list, network)) {
         throw new Error(
-          `Failed initializing network ${network}. Allowed values are ${Object.keys(this.network.list)}.`,
+          `Failed initializing network ${network}. Allowed values are ${Object.keys(
+            this.network.list
+          )}.`
         );
       }
 
-      if (checkWeb3 && (typeof window.ethereum !== 'undefined')) {
-        console.log('injected ethereum'); // eslint-disable-line no-console
+      if (checkWeb3 && typeof window.ethereum !== "undefined") {
+        console.log("injected ethereum"); // eslint-disable-line no-console
         this.web3Provider = window.ethereum;
 
         this.web3 = new Web3(this.web3Provider);
@@ -142,18 +144,17 @@ export default {
           await this.initWeb3(network, false);
         }
       } else {
-        console.log('provided ethereum'); // eslint-disable-line no-console
+        console.log("provided ethereum"); // eslint-disable-line no-console
         this.network.current = this.network.list[network];
-        this.web3Provider = new Web3.providers.HttpProvider(this.network.list[network].web3Provider);
+        this.web3Provider = new Web3.providers.HttpProvider(
+          this.network.list[network].web3Provider
+        );
         this.web3 = new Web3(this.web3Provider);
       }
     },
-    initContract (address) {
+    initContract(address) {
       console.log(`init ${address} on ${this.network.current.name}`); // eslint-disable-line no-console
-      this.instances.token = new this.web3.eth.Contract(
-        TokenABI,
-        address,
-      );
+      this.instances.token = new this.web3.eth.Contract(TokenABI, address);
     },
   },
 };
